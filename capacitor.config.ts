63fcc7f7b5431cd83932bec4928981f929fdc972
unknown <kosttoonek7@gmail.com>
2026-05-20 17:17:14 +0300

    Add Capacitor Android support and build configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.minar.goapp',
+  appName: 'minar-go-app',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
